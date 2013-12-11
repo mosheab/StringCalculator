6609e42d5c52ba76f13b7c487cdc63fab0c3204a
mosheab <moshe.uni@gmail.com>
2013-12-11 19:22:57 +0200

    first commit - adding the StringCalculatorTester (JUnit) and the string calculator class

---
+++ TDD/src/calculator/stringCalculatorTest.java
@@ -0,0 +1,14 @@
+package calculator;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class stringCalculatorTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
